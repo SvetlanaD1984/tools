4d01d6a5b3281693d5917094ec817bccb391bb20
svetlana <developer-svetlana@yandex.ru>
2021-12-13 19:42:49 +0500

    dz2

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+import "./pages/timer.js"; 
+import "./pages/datecalc.js"; 
+//import "./pages/switcher.js";
+import "./libs/howler.js"; 
+import "../css/main.css";
+//import "../scss/main.scss";
+
+//import jpg from "../1.jpg";
